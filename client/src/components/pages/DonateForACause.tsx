93c57eef812ea69064b299f09c5e2dbafcae052c
prathamesh-07 <prathameshchiddarwar42@gmail.com>
2025-07-01 17:59:22 +0530

    feat(ui): added header and link pages

---
+++ client/src/components/pages/DonateForACause.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DonateForACause = () => {
+  return (
+    <div>
+      <h1>Donate for a Cause Page</h1>
+    </div>
+  );
+};
+
+export default DonateForACause; 
---
+++ client/src/components/pages/NewsEvents.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsEvents = () => {
+  return (
+    <div>
+      <h1>News & Events Page</h1>
+    </div>
+  );
+};
+
+export default NewsEvents; 
