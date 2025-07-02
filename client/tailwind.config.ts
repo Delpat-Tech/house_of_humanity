4fb1fdb6bca967b71c23093a8e16707492a4efe3
Kaushiki-02 <kaushikiagrawal283@gmail.com>
2025-07-02 21:32:36 +0530

    feat(ui): integrate tailwindcss with custom color config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -6,7 +6,13 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF69B4', // Hot Pink
+        'primary-dark': '#C71585', // Medium Violet Red, a darker shade for hover states or active elements
+        // Add more custom colors here as needed
+      },
+    },
   },
   plugins: [],
 }
