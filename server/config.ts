333c4bf1a0a3bd7c25493c28911a95d0c1a3ad68
Jyoti <jyotisingh016@gmail.com>
2025-08-19 14:47:19 +0530

    chore: mofify config file to load env properly

--- server/config.ts
+++ server/config.ts
@@ -8,18 +8,3 @@ dotenv.config({ path: path.resolve(__dirname, '..', envFile) });
 
 console.log(`✅ Loaded environment variables from ${envFile}`);
 
-// Sanity check for required vars
-if (!process.env.NGO_EMAIL) {
-  console.error(`❌ Missing NGO_EMAIL in ${envFile}`);
-  process.exit(1);
-}
-
-export const config = {
-  port: process.env.PORT || 3000,
-  email: process.env.NGO_EMAIL,
-  emailUser: process.env.EMAIL_USER,
-  ngoEmailPass: process.env.NGO_EMAIL_PASS,
-  razorpayKeyId: process.env.RAZORPAY_KEY_ID,
-  razorpayKeySecret: process.env.RAZORPAY_KEY_SECRET,
-};
-
